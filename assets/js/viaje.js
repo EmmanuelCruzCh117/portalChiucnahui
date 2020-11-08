ff13f13b714bf6fcc4258a107ff2d3f386ae6640
emany <viajero28417@innovaccion.mx>
2020-11-08 05:10:35 -0600

    Hola

---
+++ assets/js/viaje.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#summit").click(function(e) {
+        e.preventDefault();
+
+        var travel = $("#viaje").val().trim();
+
+        console.log(travel);
+
+        mostrarDato(travel);
+
+    });
+
+
+    async function mostrarDato(travel) {
+
+        var travel1 = "Tlalocan";
+        var travel2 = "Valle del mezquital";
+        var travel3 = "Chichihuacuauhco";
+        var travel4 = "Mictlán"
+
+        if (travel == travel1) {
+            location.href = "tlalocan.html";
+        } else if (travel == travel2) {
+            location.href = "vm.html";
+        } else if (travel3 == travel3) {
+            location.href = "chichihua.html";
+        } else if (travel == travel4) {
+            location.href = "mict.html";
+        } else {
+            alert("Ingresa un lugar válido");
+        }
+    }
+
+});
